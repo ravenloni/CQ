a7876ec0b4acf07162cd89b5cea00b3f4f1f1728
ravenloni <sebas16m52@gmail.com>
2024-07-05 01:43:23 +0200

    version de mv

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ document.addEventListener('mouseup', () => {
         caja.style.transform = 'rotateX(0deg) rotateY(0deg)';
     }
 });
+
+//moviles
+function startDrag(e) {
+    isDragging = true;
+    startX = (e.clientX || e.touches[0].clientX) - currentX;
+    startY = (e.clientY || e.touches[0].clientY) - currentY;
+    caja.style.transition = 'none';
+}
+
+function onDrag(e) {
+    if (isDragging) {
+        currentX = (e.clientX || e.touches[0].clientX) - startX;
+        currentY = (e.clientY || e.touches[0].clientY) - startY;
+        caja.style.transform = `rotateX(${currentY / 5}deg) rotateY(${currentX / 5}deg)`;
+    }
+}
+
+function endDrag() {
+    if (isDragging) {
+        isDragging = false;
+        caja.style.transition = 'transform 1s';
+        caja.style.transform = 'rotateX(0deg) rotateY(0deg)';
+    }
+}
+
+document.addEventListener('mousedown', startDrag);
+document.addEventListener('mousemove', onDrag);
+document.addEventListener('mouseup', endDrag);
+
+document.addEventListener('touchstart', startDrag);
+document.addEventListener('touchmove', onDrag);
+document.addEventListener('touchend', endDrag);
+
